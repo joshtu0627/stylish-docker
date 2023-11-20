af38593f37325f8869e36d88517ac369a9bcef22
Josh <joshtu0627@gmail.com>
2023-11-20 20:49:45 +0800

    local

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
 
-import CartProduct from "../../../types/CartProduct";
+import CartProduct from "../types/CartProduct";
 
 export default function CartPage() {
   const [cartProducts, setCartProducts] = useState<CartProduct[]>([]);
@@ -15,13 +15,93 @@ export default function CartPage() {
     send_time: "",
   });
 
+  const [total, setTotal] = useState(0);
+
   const storage = window.localStorage;
 
   useEffect(() => {
     console.log("storage", storage.getItem("cart"));
+    TPDirect.setupSDK(
+      12348,
+      "app_pa1pQcKoY22IlnSXq5m5WP5jFKzoRG58VEXpT7wU62ud7mMbDOGzCYIlzzLF",
+      "sandbox"
+    );
+    TPDirect.card.setup({
+      // Display ccv field
+      fields: {
+        number: {
+          // css selector
+          element: "#card-number",
+          placeholder: "**** **** **** ****",
+        },
+        expirationDate: {
+          // DOM object
+          element: document.getElementById("card-expiration-date"),
+          placeholder: "MM / YY",
+        },
+        ccv: {
+          element: "#card-ccv",
+          placeholder: "ccv",
+        },
+      },
 
+      styles: {
+        // Style all elements
+        input: {
+          color: "gray",
+        },
+        // Styling ccv field
+        "input.ccv": {
+          // 'font-size': '16px'
+        },
+        // Styling expiration-date field
+        "input.expiration-date": {
+          // 'font-size': '16px'
+        },
+        // Styling card-number field
+        "input.card-number": {
+          // 'font-size': '16px'
+        },
+        // style focus state
+        ":focus": {
+          // 'color': 'black'
+        },
+        // style valid state
+        ".valid": {
+          color: "green",
+        },
+        // style invalid state
+        ".invalid": {
+          color: "red",
+        },
+        // Media queries
+        // Note that these apply to the iframe, not the root window.
+        "@media screen and (max-width: 400px)": {
+          input: {
+            color: "orange",
+          },
+        },
+      },
+      // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
+      isMaskCreditCardNumber: true,
+      maskCreditCardNumberRange: {
+        beginIndex: 6,
+        endIndex: 11,
+      },
+    });
     setCartProducts(JSON.parse(storage.getItem("cart") || "[]"));
   }, []);
+
+  useEffect(() => {
+    function countTotal() {
+      let total = 0;
+      cartProducts.forEach((product) => {
+        total += product.price * product.qty;
+      });
+      setTotal(total);
+    }
+    countTotal();
+  }, [cartProducts]);
 
   function deleteProduct(id: number) {
     console.log("id", id);
@@ -51,8 +131,27 @@ export default function CartPage() {
     storage.setItem("cart", JSON.stringify(newCartProducts));
     const event = new Event("customStorageChange");
     window.dispatchEvent(event);
-
     setCartProducts(JSON.parse(storage.getItem("cart") || "[]"));
+  }
+
+  function createPayment() {
+    const tappayStatus = TPDirect.card.getTappayFieldsStatus();
+    console.log(tappayStatus);
+
+    // Check TPDirect.card.getTappayFieldsStatus().canGetPrime before TPDirect.card.getPrime
+    if (tappayStatus.canGetPrime === false) {
+      alert("can not get prime");
+      return;
+    }
+    TPDirect.card.getPrime((result) => {
+      if (result.status !== 0) {
+        console.error("獲取 Prime 失敗:", result.msg);
+        return;
+      }
+      const prime = result.card.prime;
+      console.log("prime", prime);
+      // 使用 prime 進行支付流程，例如發送給後端服務器
+    });
   }
 
   return (
@@ -251,6 +350,83 @@ export default function CartPage() {
                   不指定
                 </label>
               </div>
+            </div>
+          </div>
+          <div className="font-bold mt-10">訂購資料</div>
+          <div className="h-0.5 bg-gray-500"></div>
+          <div className="flex-col w-3/5">
+            <div
+              className="flex w-full justify-between mt-5 tpfield"
+              id="card-number"
+            >
+              {/* <div>信用卡號碼</div> */}
+              {/* <input
+                type="text"
+                className="border border-gray-300 border-2 w-3/4 rounded-md focus:outline-none"
+              ></input> */}
+            </div>
+          </div>
+          <div className="flex-col w-3/5">
+            <div
+              className="flex w-full justify-between mt-5  tpfield"
+              id="card-expiration-date"
+            >
+              {/* <div>有效期限</div>
+              <input
+                type="text"
+                className="expiration-date border border-gray-300 border-2 w-3/4 rounded-md focus:outline-none"
+              ></input> */}
+            </div>
+          </div>
+          <div className="flex-col w-3/5">
+            <div
+              className="flex w-full justify-between my-5  tpfield"
+              id="card-ccv"
+            >
+              {/* <div>安全碼</div>
+              <input
+                type="text"
+                className="ccv border border-gray-300 border-2 w-3/4 rounded-md focus:outline-none"
+              ></input> */}
+            </div>
+          </div>
+          <div className="flex justify-end my-10">
+            <div className="w-72">
+              <div className="flex-col  font-bold">
+                <div className="flex justify-between my-3">
+                  <div>總金額</div>
+                  <div>
+                    NT.
+                    <span className="text-xl"> {total}</span>
+                  </div>
+                </div>
+                <div className="flex justify-between my-3">
+                  <div>運費</div>
+                  <div>
+                    NT.
+                    <span className="text-xl"> 30</span>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <div className="w-full h-0.5 bg-black"></div>
+                </div>
+                <div className="flex justify-between my-3">
+                  <div>應付金額</div>
+                  <div>
+                    NT.
+                    <span className="text-xl"> {total + 30}</span>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="h-16 bg-black mt-14 text-white flex justify-center items-center"
+                onClick={() => {
+                  createPayment();
+                }}
+              >
+                <div>確 認 付 款</div>
+              </div>
+              <div></div>
             </div>
           </div>
         </div>
